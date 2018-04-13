5beff7583a58f513030a5d0c453c3b7d8a6acf65
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2018-04-13 16:37:52 +0200

    added global filter and function to check them

---
+++ src/screens/settings/checkFilter.js
@@ -0,0 +1,16 @@
+function checkFilter(filter) {
+    if(filter.isLecturerMode){
+        if(filter.lecturer === null){
+            return false;
+        }
+    }else{
+        if(filter.group === null) return false;
+        if(filter.mode === null) return false;
+        if(filter.semester === null) return false;
+        if(filter.degree === null) return false;
+        if(filter.fieldOfStudy === null) return false;
+        if(filter.departament === null) return false;
+        if(filter.academicYear === null) return false;
+    }
+    return true;
+}
