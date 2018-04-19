b3bd891eca9a5f89250ed84e6496e9c003f30b46
PKacz <kaczory5@gmail.com>
2018-04-19 23:28:44 +0200

    make switch switch again

--- src/screens/settings/index.js
+++ src/screens/settings/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, Header, Right, Left, Body, Button, Icon, Item, Title, Container, CheckBox, Content, List, ListItem, Picker, Segment, Separator } from "native-base";
+import { Text, Header, Right, Left, Body, Button, Icon, Item, Title, Container, Content, List, ListItem, Picker, Segment, Separator, Switch } from "native-base";
 import PropTypes from "prop-types";
 
 class Settings extends Component {
@@ -41,99 +41,127 @@ class Settings extends Component {
           </Button>
         </Segment>
         <Content>
-          <List>
-            {/* <ListItem itemHeader>
-              <Text>Wybór grupy:</Text>
-            </ListItem> */}
+          {this.state.student ? (
+            <List>
+              <ListItem icon>
+                <Left>
+                  <Text note>Tryb prowadzącego:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Switch value={!this.state.student} onValueChange={() => this.setState({ student: !this.state.student })} />
+                </Right>
+              </ListItem>
 
-            <Separator>
-              <Text>Wybór grupy:</Text>
-            </Separator>
+              <Separator>
+                <Text>Wybór grupy:</Text>
+              </Separator>
 
-            <ListItem icon>
-              <Left>
-                <Text>Wydział:</Text>
-              </Left>
-              <Body />
-              <Right>
-                <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
-                  <Item label="WZIM" value="key0" />
-                  <Item label="Leśny" value="key1" />
-                </Picker>
-              </Right>
-            </ListItem>
+              <ListItem icon>
+                <Left>
+                  <Text>Wydział:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
+                    <Item label="WZIM" value="key0" />
+                    <Item label="Leśny" value="key1" />
+                  </Picker>
+                </Right>
+              </ListItem>
 
-            <ListItem icon>
-              <Left>
-                <Text>Kierunek:</Text>
-              </Left>
-              <Body />
-              <Right>
-                <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
-                  <Item label="Informatyka" value="key0" />
-                  <Item label="To i Owo" value="key1" />
-                </Picker>
-              </Right>
-            </ListItem>
+              <ListItem icon>
+                <Left>
+                  <Text>Kierunek:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
+                    <Item label="Informatyka" value="key0" />
+                    <Item label="To i Owo" value="key1" />
+                  </Picker>
+                </Right>
+              </ListItem>
 
-            <ListItem icon>
-              <Left>
-                <Text>Stopień:</Text>
-              </Left>
-              <Body />
-              <Right>
-                <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
-                  <Item label="inż" value="key0" />
-                  <Item label="mgr" value="key1" />
-                  <Item label="doc" value="key2" />
-                </Picker>
-              </Right>
-            </ListItem>
+              <ListItem icon>
+                <Left>
+                  <Text>Stopień:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
+                    <Item label="inż" value="key0" />
+                    <Item label="mgr" value="key1" />
+                    <Item label="doc" value="key2" />
+                  </Picker>
+                </Right>
+              </ListItem>
 
-            <ListItem icon>
-              <Left>
-                <Text>Semestr:</Text>
-              </Left>
-              <Body />
-              <Right>
-                <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
-                  <Item label="1" value="key0" />
-                  <Item label="2" value="key1" />
-                  <Item label="3" value="key2" />
-                </Picker>
-              </Right>
-            </ListItem>
+              <ListItem icon>
+                <Left>
+                  <Text>Semestr:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
+                    <Item label="1" value="key0" />
+                    <Item label="2" value="key1" />
+                    <Item label="3" value="key2" />
+                  </Picker>
+                </Right>
+              </ListItem>
 
-            <ListItem icon>
-              <Left>
-                <Text>Grupa:</Text>
-              </Left>
-              <Body />
-              <Right>
-                <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
-                  <Item label="1" value="key0" />
-                  <Item label="4" value="key1" />
-                  <Item label="B" value="key2" />
-                </Picker>
-              </Right>
-            </ListItem>
+              <ListItem icon>
+                <Left>
+                  <Text>Grupa:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ width: 140 }}>
+                    <Item label="1" value="key0" />
+                    <Item label="4" value="key1" />
+                    <Item label="B" value="key2" />
+                  </Picker>
+                </Right>
+              </ListItem>
 
-            {/* <ListItem itemHeader>
-              <Text>Inne:</Text>
-            </ListItem> */}
+              <Separator>
+                <Text>Inne:</Text>
+              </Separator>
 
-            <Separator>
-              <Text>Inne:</Text>
-            </Separator>
+              <ListItem>
+                <Left>
+                  <Text>Szybka zmiana grupy</Text>
+                </Left>
+                <Body />
+                <Switch value={this.state.fast} onValueChange={() => this.setState({ fast: !this.state.fast })} />
+              </ListItem>
+            </List>
+          ) : (
+            <List>
+              <ListItem icon>
+                <Left>
+                  <Text note>Tryb prowadzącego:</Text>
+                </Left>
+                <Body />
+                <Right>
+                  <Switch value={!this.state.student} onValueChange={() => this.setState({ student: !this.state.student })} />
+                </Right>
+              </ListItem>
 
-            <ListItem>
-              <Left>
-                <Text>Szybka zmiana grupy</Text>
-              </Left>
-              <Body />
-              <CheckBox checked={this.state.fast} onPress={() => this.setState({ fast: !this.state.fast })} />
-            </ListItem>
-          </List>
+              <ListItem icon>
+                <Left />
+                <Text>Kto?</Text>
+                <Body />
+                <Right>
+                  <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)}>
+                    <Item label="Adek" value="key0" />
+                    <Item label="ktoś inny" value="key1" />
+                  </Picker>
+                </Right>
+              </ListItem>
+            </List>
+          )}
         </Content>
       </Container>
     );
