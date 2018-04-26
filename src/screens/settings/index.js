680c4aba3c1a2d40d04ee7767ccc01ee16985d69
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2018-04-26 17:53:44 +0200

    finished style of settings page

--- src/screens/settings/index.js
+++ src/screens/settings/index.js
@@ -62,7 +62,6 @@ class Settings extends Component {
           </Body>
           <Right />
         </Header>
-        {this.state.student ? (
           <Segment>
             <Button
               first
@@ -79,144 +78,9 @@ class Settings extends Component {
               <Text>prowadzący</Text>
             </Button>
           </Segment>
-        ) : (
-          <Text />
-        )}
         <Content>
           {this.state.student ? (
             <Form>
-              <Separator>
-                <Text>Wybór grupy:</Text>
-              </Separator>
-
-              <Item stackedLabel>
-                <Label>
-                  <Text>Wydział:</Text>
-                </Label>
-                <Picker
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}
-                  style={{ alignSelf: "stretch" }}
-                >
-                  <Item label="WZIM" value="key0" />
-                  <Item label="Leśny" value="key1" />
-                </Picker>
-              </Item>
-
-              <Item stackedLabel>
-                <Label>
-                  <Text>Kierunek:</Text>
-                </Label>
-                <Picker
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}
-                  style={{ alignSelf: "stretch" }}
-                >
-                  <Item label="Informatyka" value="key0" />
-                  <Item label="To i Owo" value="key1" />
-                </Picker>
-              </Item>
-
-              <Item stackedLabel>
-                <Label>
-                  <Text>Stopień:</Text>
-                </Label>
-                <Picker
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}
-                  style={{ alignSelf: "stretch" }}
-                >
-                  <Item label="inż" value="key0" />
-                  <Item label="mgr" value="key1" />
-                  <Item label="doc" value="key2" />
-                </Picker>
-              </Item>
-
-              <Item stackedLabel>
-                <Label>
-                  <Text>Semestr:</Text>
-                </Label>
-                <Picker
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}
-                  style={{ alignSelf: "stretch" }}
-                >
-                  <Item label="1" value="key0" />
-                  <Item label="2" value="key1" />
-                  <Item label="3" value="key2" />
-                </Picker>
-              </Item>
-
-              <Item stackedLabel>
-                <Label>
-                  <Text>Tryb:</Text>
-                </Label>
-                <Picker
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}
-                  style={{ alignSelf: "stretch" }}
-                >
-                  <Item label="stacjonarny" value="key0" />
-                  <Item label="zębaty" value="key1" />
-                  <Item label="B" value="key2" />
-                </Picker>
-              </Item>
-
-              <Item stackedLabel>
-                <Label>
-                  <Text>Grupa:</Text>
-                </Label>
-                <Picker
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}
-                  style={{ alignSelf: "stretch" }}
-                >
-                  <Item label="1" value="key0" />
-                  <Item label="4" value="key1" />
-                  <Item label="B" value="key2" />
-                </Picker>
-              </Item>
-
-              <Separator>
-                <Text>Inne:</Text>
-              </Separator>
-
-              <ListItem>
-                <Left>
-                  <Text>Szybka grupy:</Text>
-                </Left>
-                <Body />
-                <Switch
-                  value={this.state.fast}
-                  onValueChange={() =>
-                    this.setState({ fast: !this.state.fast })
-                  }
-                />
-              </ListItem>
-            </Form>
-          ) : (
-            <Form>
-              <ListItem icon>
-                <Left>
-                  <Text note>Tryb prowadzącego:</Text>
-                </Left>
-                <Body />
-                <Right>
-                  <Switch
-                    value={!this.state.student}
-                    onValueChange={() =>
-                      this.setState({ student: !this.state.student })
-                    }
-                  />
-                </Right>
-              </ListItem>
-
               <Separator>
                 <Text>Wybór grupy:</Text>
               </Separator>
@@ -321,7 +185,7 @@ class Settings extends Component {
 
               <ListItem>
                 <Left>
-                  <Text note>Szybka zmiana prowadzącego:</Text>
+                  <Text note>Szybka grupy:</Text>
                 </Left>
                 <Body />
                 <Switch
@@ -332,19 +196,18 @@ class Settings extends Component {
                 />
               </ListItem>
             </Form>
-
-            //   <ListItem icon>
-            //     <Left />
-            //     <Text>Kto?</Text>
-            //     <Body />
-            //     <Right>
-            //       <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)}>
-            //         <Item label="Adek" value="key0" />
-            //         <Item label="ktoś inny" value="key1" />
-            //       </Picker>
-            //     </Right>
-            //   </ListItem>
-            // </List>
+          ) : (
+            <Form> 
+               <Item stackedLabel>
+                <Label>
+                  <Text note>Kto?</Text>
+                </Label>
+                <Picker mode="dropdown" selectedValue={this.state.selected1} onValueChange={this.onValueChange.bind(this)} style={{ alignSelf: "stretch" }}>
+                  <Item label="Adek" value="key0" />
+                  <Item label="ktoś inny" value="key1" />
+                </Picker>
+              </Item>
+            </Form>
           )}
         </Content>
       </Container>
