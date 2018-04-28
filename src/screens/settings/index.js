be0a4eae111170af6c205bc789b0ca46573fa2ac
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2018-04-28 14:09:49 +0200

    fix and redundant file deletion

--- src/screens/settings/index.js
+++ src/screens/settings/index.js
@@ -18,10 +18,9 @@ import {
   Separator,
   Switch,
   Form,
-  Label,
-  View, 
-  Image
+  Label
 } from "native-base";
+import { View, Image } from "react-native";
 import PropTypes from "prop-types";
 import { checkFilter } from "../settings/checkFilter";
 
